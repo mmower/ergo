d797491bfc1a04f993413c8a6fafda7a05d558a0
Matthew Mower <matt@theartofnavigation.co.uk>
2021-07-17 14:17:46 +0100

    Added ellipsize function to Ergo.Utils to truncate strings nicely.

--- lib/ergo/utils.ex
+++ lib/ergo/utils.ex
@@ -13,4 +13,29 @@ defmodule Ergo.Utils do
     List.to_string([c])
   end
 
+  @doc ~S"""
+  If the string `str` is longer than `max` (default: 40) it is truncated to 37 chars and "..." is added to the end, otherwise it is returned unaltered.
+
+  ## Examples
+
+      iex> Ergo.Utils.ellipsize("frob")
+      "frob"
+
+      iex> Ergo.Utils.ellipsize("123456789012345678901234567890123456789012345678901")
+      "1234567890123456789012345678901234567..."
+
+      iex> Ergo.Utils.ellipsize("12345678901234567890", 10)
+      "1234567..."
+
+      iex> Ergo.Utils.ellipsize("12345", 4)
+      "1..."
+  """
+  def ellipsize(str, max \\ 40) when is_binary(str) and max > 3 do
+    if String.length(str) > max do
+      String.slice(str, 0..(max-4)) <> "..."
+    else
+      str
+    end
+  end
+
 end
